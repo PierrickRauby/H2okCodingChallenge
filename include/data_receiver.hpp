7ec6773f8e8d5a888d59a0419ec135d6dceae917
PierrickRauby <pierrick.rauby@gadz.org>
2025-02-17 21:30:30 -0500

    fixed logging received data and sorting on timestamp

--- include/data_receiver.hpp
+++ include/data_receiver.hpp
@@ -6,6 +6,18 @@
 #include <vector>
 #include <mutex>
 #include <memory>
+#include <queue>
+#include <fstream>
+
+// to store the data recieved by the receiver 
+struct DataToLog {
+    long long timestamp;
+    std::string data;
+    // Sort on time stamp
+    bool operator<(const DataToLog& other) const {
+        return timestamp > other.timestamp; 
+    }
+};
 
 class DataReceiver {
 public:
@@ -17,11 +29,18 @@ private:
     boost::asio::io_context io_context;
     boost::asio::ip::tcp::acceptor acceptor;
     bool data_receiver_running;
+    // for ordering incoming data, queue and mutex
+    std::priority_queue<DataToLog> data_log_queue;
+    std::mutex data_queue_mutex;
 
-    // void accept_connections();
+       // void accept_connections();
     void start_accept();
     void handle_client(std::shared_ptr<boost::asio::ip::tcp::socket> socket);
+    // TODO: old version below, cleanup!!
     // void handle_client(boost::asio::ip::tcp::socket socket);
+    //write data to log file
+    void write_logs();
+    long long extract_timestamp(const std::string& message);
 };
 
 #endif // DATA_RECEIVER_HPP
--- src/data_receiver.cpp
+++ src/data_receiver.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <chrono>
 
 using boost::asio::ip::tcp;
 constexpr int PORT = 12345;
@@ -16,7 +19,7 @@ void DataReceiver::run() {
     data_receiver_running = true;
 
     // Start accepting clients asynchronously, prevent blokcing
-    start_accept();
+   start_accept();
 
     // asio loop
     io_context.restart();
@@ -29,7 +32,7 @@ void DataReceiver::start_accept() {
     if (!data_receiver_running) return;
     // New connection for potential new client 
     auto socket = std::make_shared<tcp::socket>(io_context);
-    //start async accept for new client to connect 
+    //start async accept for new client to connect
     acceptor.async_accept(*socket, [this, socket](const boost::system::error_code& error) {
         if (!error && data_receiver_running) {
             // on connection check that the receiver is still supposed to run 
@@ -58,6 +61,15 @@ void DataReceiver::handle_client(std::shared_ptr<tcp::socket> socket) {
                 std::cerr << "[INFO] Client disconnected.\n";
                 break;
             }
+            //Get time stamp
+            long long timestamp = extract_timestamp(received_data);
+            {
+                // write to the the data queue
+                std::lock_guard<std::mutex> lock(data_queue_mutex);
+                data_log_queue.push({timestamp, received_data});
+            }
+            // Write logs now
+            write_logs();
             //output data and ACK
             std::cout << "[RECEIVED] " << received_data << std::endl;
             boost::asio::write(*socket, boost::asio::buffer("ACK\n"));
@@ -67,11 +79,61 @@ void DataReceiver::handle_client(std::shared_ptr<tcp::socket> socket) {
     }
 }
 
+void DataReceiver::write_logs() {
+
+    std::lock_guard<std::mutex> lock(data_queue_mutex);
+    // append to log files
+    std::fstream log_file("data.log", std::ios::app);
+    //TODO: Check that log file would be created if doesn't exist
+
+    //write the entire data queue
+    while (!data_log_queue.empty()) {
+        DataToLog entry = data_log_queue.top();
+        data_log_queue.pop();
+        log_file << entry.data << std::endl;
+    }
+    log_file.close();
+}
+
+long long DataReceiver::extract_timestamp(const std::string& message) {
+    size_t pos = message.find("\"timestamp\":");
+    if (pos != std::string::npos) {
+        pos += 11;  // Move past `"timestamp":`
+        // Skip any whitespace between `timestamp:` and the number
+        while (pos < message.size() && (message[pos] == ' ' || message[pos] == ':')) {
+            pos++;
+        }
+        // Find the end of the timestamp (first non-digit character)
+        size_t end = message.find_first_not_of("0123456789", pos);
+        if (end == std::string::npos) {
+            end = message.size();  
+        }
+        // get timestamp as a string
+        std::string timestamp_str = message.substr(pos, end - pos);
+
+        if (!timestamp_str.empty()) {
+            try {
+                return std::stoll(timestamp_str);
+            } catch (const std::exception& e) {
+                std::cerr << "[ERROR] Failed to convert timestamp: " << e.what() << std::endl;
+            }
+        } else {
+            std::cerr << "[ERROR] Extracted empty timestamp!" << std::endl;
+        }
+    } else {
+        std::cerr << "[ERROR] No timestamp found in message!" << std::endl;
+    }
+    // fail safe, if we don't have a timestamp, use now
+    return std::chrono::system_clock::now().time_since_epoch() / std::chrono::milliseconds(1);
+}
+
+
 void DataReceiver::stop() {
     if (!data_receiver_running) return;
     //Cleanup
     data_receiver_running = false;
     acceptor.close();
     io_context.stop();
+    //TODO: is the data_log_queue empty?
     Logger::log("[INFO] Data Receiver stopping...");
 }
